90929c4a39e07beb93c85857121dc27dc114b206
PHOENIX\NavidG <ngolforoushan@aask-az.org>
2017-07-06 16:15:43 -0700

    Scurity Project build.

---
+++ CSharpMania/Security/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Security
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
