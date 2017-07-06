97579ecabee577592e26cf35b66b791658e77698
PHOENIX\NavidG <ngolforoushan@aask-az.org>
2017-07-06 16:42:39 -0700

    init security

--- CSharpMania/Security/Program.cs
+++ CSharpMania/Security/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace Security
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Security {
+    class Program {
+        static void Main(string[] args) {
+            loop();
+        }
+
+        static void loop() {
+            Console.WriteLine(@ "QSelect...");
+            Console.WriteLine("Password Hash");
+            Console.WriteLine("1.Simple Task");
+            Console.WriteLine("2.");
+            Console.WriteLine("3.");
+            Console.WriteLine("4.");
+            var sel = Convert.ToInt32(Console.ReadLine());
+
+            switch (sel) {
+                case 1:
+                    PasswordHash();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void PasswordHash() {
+            System.Console.WriteLine("Passwor is newTestPscm");
+            System.Console.WriteLine("Hash is..");
+            
         }
     }
 }
